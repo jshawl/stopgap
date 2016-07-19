2f62871c3858ad9827ace77e7f40de17cb8493f5
Jesse Shawl <jesse@jshawl.com>
2016-07-19 17:41:16 -0400

    reset

--- models/resource.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require("mongoose")
-var resources = require("../resources")
-var ResourceSchema = new mongoose.Schema({ })
-var Resource = mongoose.model('Resource',ResourceSchema)
-module.exports = Resource
