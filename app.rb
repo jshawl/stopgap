90142b56d93ac2f3f7ad13de1928bc5ee28b919a
Jesse Shawl <jesse@jshawl.com>
2016-07-21 09:11:13 -0400

    hardcode https

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ end
 
 helpers do
   def url
-    "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}".gsub(/\?(.*)/,'')
+    "https://#{request.env['HTTP_HOST']}".gsub(/\?(.*)/,'')
   end
   def path
     request.path[1..-1]
