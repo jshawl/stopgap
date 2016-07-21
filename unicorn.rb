fe64f9e627a036ad56c62b066a6cde8937a3a216
Jesse Shawl <jesse@jshawl.com>
2016-07-21 06:35:16 -0400

    deployed

---
+++ unicorn.rb
@@ -0,0 +1,17 @@
+@dir = "/var/www/stopgap.store/"
+
+worker_processes 2
+working_directory @dir
+
+timeout 30
+
+# Specify path to socket unicorn listens to,
+# we will use this in our nginx.conf later
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
